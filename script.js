24796b134bf528058fa644af2ba86585bf5ded23
bjornum <bjoern-einar2@hotmail.no>
2025-02-24 19:17:22 +0100

    Clone of Assessment
    Code to fix copied from the codepen link provided.
    Link: https://codepen.io/james_marriott/pen/PovaQrg

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Script works");
+var saturnImage = document.querySelector("img");
+saturnImage.onmouseover = function () {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+};
+
+var closeBtn = document.getElementsByClassName("close")[0];
+closeBtn.onclick = function () {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  var modal = document.getElementById("myModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
